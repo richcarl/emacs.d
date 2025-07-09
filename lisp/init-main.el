ec1f2893b335bf29493fecc55c18273453ee49a7
Richard Carlsson <carlsson.richard@gmail.com>
2025-07-09 17:56:38 +0200

    Switch from direnv to mise

--- lisp/init-main.el
+++ lisp/init-main.el
@@ -63,9 +63,9 @@
 (require 'init-coding)
 (require 'init-misc)
 
-;; Enable direnv mode last of all
-(use-package envrc
-  :hook (after-init . envrc-global-mode))
+;; Enable mise last of all (replaces envrc)
+(use-package mise
+  :hook (after-init . global-mise-mode))
 
 (provide 'init-main)
 ;;; init-main.el ends here
