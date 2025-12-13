bacfc1d75f6bddde2b4043b411bf8541368cc493
Richard Carlsson <carlsson.richard@gmail.com>
2025-12-13 11:52:21 +0100

    Stop using `helpful` package - too slow

--- lisp/init-addons.el
+++ lisp/init-addons.el
@@ -27,16 +27,6 @@
   (add-hook 'after-init-hook 'editorconfig-mode))
 
 
-;; Improved help
-(use-package helpful
-  :config
-  (global-set-key (kbd "C-h f") #'helpful-callable)
-  (global-set-key (kbd "C-h v") #'helpful-variable)
-  (global-set-key (kbd "C-h k") #'helpful-key)
-  (global-set-key (kbd "C-h x") #'helpful-command)
-  (global-set-key (kbd "C-c C-d") #'helpful-at-point))
-
-
 ;; Terminal (Eat: Emulate-a-terminal)
 ;; note: run 'eat-compile-terminfo' if keys are not working properly
 (use-package eat
